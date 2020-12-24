4900d08379cd54c07c9c5c58437e89c941e28b47
bph <birgit.pauli@gmail.com>
2020-12-24 15:52:20 -0500

    first commit

---
+++ index.child.process.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+console.log(`Hi there.`);
+
+//const { spawn } = require('child_process');
+//const lsCommand = spawn('ls');
+// console.log(lsCommand);
+// we would want to the data from the child process
+// printed on screen. 
+//lsCommand.stdout.on('data',(data) => {
+  //  console.log(`Data from child process: \n${data}`)
+//});
+
+//Now we want to create a directory so we need to execute a child process
+const { exec } = require('child_process');
+
+//exec('"/path/to/test file/test.sh" arg1 arg2'); <- documentation
+
+//exec('mkdir utils'); <- now this hard codes the directory name, which we might now want. 
+// let's use a variable name
+
+
+const dirName = process.argv[2];
+
+exec(`mkdir -p ${dirName}`);
